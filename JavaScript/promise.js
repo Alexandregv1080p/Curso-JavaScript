112eee96ec92322048f3c3abdb8f37c3780c07fa
Alexandre Gomes Vieira <84604919+Alexandregv1080p@users.noreply.github.com>
2022-09-26 15:23:43 -0300

    Update promise.js

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -1,5 +1,18 @@
 let a = 1
 
+//um promessa gera um unico valor
+//a n ser q seja um objeto
 let p = new Promise(function(cumprirPromessa){
-     
+     cumprirPromessa({
+        x:3,
+        y:4
+     })
+})
+
+//console.log(typeof p) -> object
+
+//quando a promessa for cumprida
+
+p.then(function(valor){
+    console.log(valor)
 })
