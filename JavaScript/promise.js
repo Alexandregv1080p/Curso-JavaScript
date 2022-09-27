6ff065495441afa832d57ec8143b4bf2ed6c3f35
Alexandregv1080p <alexandregv1999@gmail.com>
2022-09-27 07:37:23 -0300

    Update promise.js

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -1,18 +1,34 @@
+const { listenerCount } = require("process")
+
 let a = 1
 
 //um promessa gera um unico valor
 //a n ser q seja um objeto
-let p = new Promise(function(cumprirPromessa){
-     cumprirPromessa({
-        x:3,
-        y:4
-     })
-})
+//let p = new Promise(function(cumprirPromessa){
+//     cumprirPromessa({
+//        x:3,
+//        y:4
+//     })
+//})
 
 //console.log(typeof p) -> object
 
-//quando a promessa for cumprida
-
-p.then(function(valor){
-    console.log(valor)
+let p = new Promise(function(cumprirPromessa){
+    cumprirPromessa(['Ana','Bia','Carlos','Vander'])
+    
 })
+
+
+//quando a promessa for cumprida .then
+//
+    //.then(valor => valor[0])//pega o primeiro item do array
+    //.then(valor => console.log(valor))//exibe o primeiro item
+    //.then(primeiroDoArray())//pega o primeiro elemento do primeiro item
+    //.then(letra => letra.toLowerCase())//transforma o primeiro elemento em minuscula
+    //.then(minuscula => console.log(minuscula))
+//
+let primeiroDoArray = (array)=>{
+    return array[0]
+}
+p
+    .then(valor => valor[0])
